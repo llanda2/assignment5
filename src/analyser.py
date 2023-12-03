fb319b5e55217599822635e2e01e2f4211e9cee9
Lauren Landa <laurenlanda@Lauren-Landa-MacBook-Pro.local>
2023-12-03 14:12:39 -0800

    uhh, not connecting to data

--- src/analyser.py
+++ src/analyser.py
@@ -1,15 +1,16 @@
+import gcsfs
 from pyspark.sql import SparkSession
 from google.cloud import storage
 from io import BytesIO
+import json
+#Hmmm why no work
+#link used: https://stackoverflow.com/questions/58708081/how-to-read-json-file-in-python-code-from-google-cloud-storage-bucket
+gcs_file_system = gcsfs.GCSFileSystem(project="AssignmentFive")
+gcs_json_path = "gs://laurenbucketyoutube/history/watch-history.json"
+with gcs_file_system.open(gcs_json_path) as f:
+    json_dict = json.load(f)
 
-# Set up a Spark session
-spark = SparkSession.builder.appName("GCSExample").getOrCreate()
-# GCS bucket and object details
-bucket_name = "laurenbucketyoutube"
-object_name = "watch-history.json"
 
-
-# Function to read JSON data from GCS
 # Function to read JSON data from GCS
 def read_gcs_data(bucket_name, object_name):
     client = storage.Client()
