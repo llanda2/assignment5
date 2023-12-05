b448b87bc76e956f5f710a87b1d25d602a58e733
Lauren Landa <laurenlanda@Lauren-Landa-MacBook-Pro.local>
2023-12-05 14:44:30 -0800

    uh

--- src/runner.py
+++ src/runner.py
@@ -17,7 +17,7 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 with open('/Users/laurenlanda/Desktop/Takeout/YouTube and YouTube Music/history/watch-history.json', 'r') as file:
     watch_history = json.load(file)
 
-video_ids = [item['contentDetails']['videoId'] for item in watch_history['items']]
+video_ids = [item.get('contentDetails', {}).get('videoId', '') for item in watch_history]
 
 # Get video details to analyze genres
 video_details = youtube.videos().list(part='snippet', id=','.join(video_ids)).execute()
