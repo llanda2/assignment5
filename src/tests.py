8a55ec5097d42dd19ef088188a4bdb41535b0f4d
Lauren Landa <laurenlanda@Lauren-Landa-MacBook-Pro.local>
2023-12-07 16:38:32 -0800

    uhhh more code testing

--- src/tests.py
+++ src/tests.py
@@ -1,24 +1,101 @@
+# import json
+# import os
+# import re
+# import pandas as pd
+# from imblearn.over_sampling import RandomOverSampler
+# from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+# from googleapiclient.discovery import build
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.pipeline import make_pipeline
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize
+#
+# # Set up API key
+# api_key = 'AIzaSyBB__ugeyjrQxGceHF1S6o0PIqBoOfWD4E'
+#
+# # Fetch video categories using OAuth
+# scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+#
+# # Define stop_words
+# stop_words = set(stopwords.words('english'))
+#
+#
+# # Function to extract video ID from titleUrl
+# def extract_video_id(url):
+#     if isinstance(url, str):  # Check if 'url' is a string
+#         match = re.search(r'v=([a-zA-Z0-9_-]+)', url)
+#         return match.group(1) if match else None
+#     return None
+#
+#
+# # Function to preprocess text
+# def preprocess_text(text):
+#     if isinstance(text, str):  # Check if 'text' is a string
+#         words = word_tokenize(text)
+#         words = [word.lower() for word in words if word.isalpha() and word.lower() not in stop_words]
+#         return ' '.join(words)
+#     return ''
+#
+#
+# # Function to fetch video categories
+# def fetch_video_categories(api_key):
+#     youtube = build('youtube', 'v3', developerKey=api_key)
+#     request = youtube.videoCategories().list(part="snippet", regionCode="US")
+#     response = request.execute()
+#     categories = {item['id']: item['snippet']['title'] for item in response['items']}
+#     return categories
+#
+#
+# # Fetch video categories
+# video_categories = fetch_video_categories(api_key)
+#
+# # Print video categories for debugging
+# print("Video Categories:", video_categories)
+# # Load JSON data
+# with open('/Users/laurenlanda/Desktop/Takeout/YouTube and YouTube Music/history/watch-history.json', 'r') as file:
+#     video_data = json.load(file)
+#
+# # Create a DataFrame from the JSON data
+# df = pd.DataFrame(video_data)
+#
+# # Extract video ID from titleUrl
+# df['VideoID'] = df['titleUrl'].apply(extract_video_id)
+#
+# # Preprocess video texts
+# df['VideoText'] = df['title'] + ' ' + df.get('description', '')
+# df['VideoText'] = df['VideoText'].apply(preprocess_text)
+#
+# # Create a new column for video categories using the previously fetched categories
+# df['VideoCategory'] = df['VideoID'].replace(video_categories)
+#
+# # Debug prints for checking the mapping
+# print("Mapping of VideoID to VideoCategory:")
+# print(df[['VideoID', 'VideoCategory']].head())
+# print("Unique Video IDs After Mapping:", df['VideoID'].unique())
+#
+# # Debug print for checking video_categories
+# print("Fetched Video Categories:", video_categories)
+# # Debug print to compare VideoID values
+# print("VideoID values in DataFrame:", df['VideoID'].unique())
+# print("VideoID values in video_categories:", video_categories.keys())
+
 import json
 import os
-from collections import Counter
-from random import random
-import random as rand_module
-from imblearn.over_sampling import RandomOverSampler
+import re
 import pandas as pd
+from imblearn.over_sampling import RandomOverSampler
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from googleapiclient.discovery import build
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split
+from googleapiclient.discovery import build
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# import google_auth_oauthlib.flow
-# import googleapiclient.discovery
-# import googleapiclient.errors
-
-# Set up  API key
+# Set up API key
 api_key = 'AIzaSyBB__ugeyjrQxGceHF1S6o0PIqBoOfWD4E'
 
 # Fetch video categories using OAuth
@@ -28,6 +105,24 @@ scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 stop_words = set(stopwords.words('english'))
 
 
+# Function to extract video ID from titleUrl
+def extract_video_id(url):
+    if isinstance(url, str):  # Check if 'url' is a string
+        match = re.search(r'v=([a-zA-Z0-9_-]+)', url)
+        return match.group(1) if match else None
+    return None
+
+
+# Function to preprocess text
+def preprocess_text(text):
+    if isinstance(text, str):  # Check if 'text' is a string
+        words = word_tokenize(text)
+        words = [word.lower() for word in words if word.isalpha() and word.lower() not in stop_words]
+        return ' '.join(words)
+    return ''
+
+
+# Function to fetch video categories
 def fetch_video_categories(api_key):
     youtube = build('youtube', 'v3', developerKey=api_key)
     request = youtube.videoCategories().list(part="snippet", regionCode="US")
@@ -39,51 +134,45 @@ def fetch_video_categories(api_key):
 # Fetch video categories
 video_categories = fetch_video_categories(api_key)
 
-# Load YouTube watch history from a JSON file
+# Print video categories for debugging
+print("Video Categories:", video_categories)
+
+# Load JSON data
 with open('/Users/laurenlanda/Desktop/Takeout/YouTube and YouTube Music/history/watch-history.json', 'r') as file:
     video_data = json.load(file)
 
-# Extract relevant information
-video_texts = [video['title'] + ' ' + video.get('description', '') for video in video_data]
+# Create a DataFrame from the JSON data
+df = pd.DataFrame(video_data)
 
+# Extract video ID from titleUrl
+df['VideoID'] = df['titleUrl'].apply(extract_video_id)
 
-def preprocess_text(text):
-    words = word_tokenize(text)
-    words = [word.lower() for word in words if word.isalpha() and word.lower() not in stop_words]
-    return ' '.join(words)
+# Preprocess video texts
+df['VideoText'] = df['title'] + ' ' + df.get('description', '')
+df['VideoText'] = df['VideoText'].apply(preprocess_text)
 
+# Create a new column for video categories using the previously fetched categories
+df['VideoCategory'] = df['VideoID'].replace(video_categories)
 
-video_texts = [preprocess_text(text) for text in video_texts]
+# Encode the 'VideoCategory' column into numerical labels
+label_encoder = LabelEncoder()
+df['Category_Label'] = label_encoder.fit_transform(df['VideoCategory'])
 
-# Assuming the key for video ID is 'id' in each dictionary within video_data
-video_category_mapping = {video.get('id', 'Unknown'): video_categories.get(video.get('id'), 'Unknown') for video in video_data}
+# Train-test split for the classifier
+X_train, X_test, y_train, y_test = train_test_split(df['VideoText'], df['VideoCategory'], test_size=0.2, random_state=42)
 
-# Create a DataFrame with video text and corresponding category
-df = pd.DataFrame({'Text': video_texts, 'Category': [video_category_mapping.get(video.get('id'), 'Unknown') for video in video_data]})
-
-# Label Encoding
-df['Category_Label'] = pd.Categorical(df['Category']).codes
-
-# Train-test split
-X_train, X_test, y_train, y_test = train_test_split(df['Text'], df['Category_Label'], test_size=0.2, random_state=42)
-
-# Use RandomOverSampler to balance classes
-sampler = RandomOverSampler(random_state=42)
-X_resampled, y_resampled = sampler.fit_resample(X_train.values.reshape(-1, 1), y_train)
-X_train_resampled = pd.Series(X_resampled.flatten())
-
-# Text classification model (Naive Bayes as an example)
+# Create a pipeline with a TF-IDF vectorizer and a Multinomial Naive Bayes classifier
 model = make_pipeline(TfidfVectorizer(), MultinomialNB())
-model.fit(X_train_resampled, y_resampled)
 
-# Predict the category for the test set
+# Train the model on the training set
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
 y_pred = model.predict(X_test)
 
 # Evaluate the model
-accuracy = accuracy_score(y_test, y_pred)
-classification_rep = classification_report(y_test, y_pred)
-confusion_mat = confusion_matrix(y_test, y_pred)
-#
-# print(f"Accuracy: {accuracy}")
-# print("Classification Report:\n", classification_rep)
-# print("Confusion Matrix:\n", confusion_mat)
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
+print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
